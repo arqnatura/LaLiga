d4566d347979109870831079ac14e900b989a875
Angel <Angel@DESKTOP-1PIPS59>
2019-03-20 09:58:28 +0000

    20 de marzo de 2019

--- LaLiga/src/control/Ficheros.java
+++ LaLiga/src/control/Ficheros.java
@@ -1,11 +1,38 @@
 package control;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.Scanner;
 
 public class Ficheros {
 
+	// Este método recorre recursivamente directorios
+	
+	public static void traverse(File parentNode, String leftIndent) {
+	    if (parentNode.isDirectory()) {
+	        System.out.println(leftIndent + parentNode.getName());
+
+	        leftIndent += "     ";
+
+	        File childNodes[] = parentNode.listFiles();
+	        for (File childNode : childNodes) {
+	            traverse(childNode, leftIndent);
+	        }
+	    } else {
+
+	        System.out.println(leftIndent +"|   --> "+ parentNode.getName());
+
+	    }
+	}
+	
+	
 	
 	public void lecturaFicheros () {
 		
@@ -59,14 +86,48 @@ public class Ficheros {
 	}
 }
 	
+	public static void escrituraFicheros2 () {
+
+		String[] lineas = { "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "..." };
+
+		/** FORMA 2 DE ESCRITURA. Con el fichero codificado en UTF-8 **/
+		Writer out = null;
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("fichero_escritura2.txt"), "UTF-8"));
+			
+			// Escribimos linea a linea en el fichero
+			for (String linea : lineas) {
+				try {
+					out.write(linea+"\n");
+				} catch (IOException ex) {
+					System.out.println("Mensaje excepcion escritura: " + ex.getMessage());
+				}
+			}
+
+		} catch (UnsupportedEncodingException | FileNotFoundException ex2) {
+			System.out.println("Mensaje error 2: " + ex2.getMessage());
+		} finally {
+			try {
+				out.close();
+			} catch (IOException ex3) {
+				System.out.println("Mensaje error cierre fichero: " + ex3.getMessage());
+			}
+		}
+	
+	}
+	
 	
 		
 	public static void main(String[] args) {
 		Ficheros ejercicio = new Ficheros();
 		
-		// ejercicio.lecturaFicheros();
-
-		ejercicio.escrituraFicheros();
+		//Recorre el diectorio
+	    //	File inputFolder = new File("c:/");
+	    //	traverse(inputFolder, "");
+		
+		
+		// ejercicio.lecturaFicheros2();
+		// ejercicio.escrituraFicheros2();
 	}
 
 }
